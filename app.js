a67bca6e244786c453d27f4306a0380d4b0b6305
Patryck Yandell Jimenez Ogando <patryckyandelljimenez@gmail.com>
2025-08-23 22:52:55 -0400

    Prueba CI/CD

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hola Mundo desde DevOps! - versión prueba');
+  res.send('Hola Mundo desde DevOps!');
 });
 
 module.exports = app;
